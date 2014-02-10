000ee46c7f4463c475f834d9ca28c816ac12b86b
Ian White <ianwhite@utexas.edu>
2014-02-10 11:13:28 -0600

    Add keyword argument vegetarian: false to Topping.initialize. Add :vegetarian to attr_accessor.

--- pizza.rb
+++ pizza.rb
@@ -2,15 +2,16 @@ class Pizza
 end
 
 class Topping
-  attr_accessor :name
+  attr_accessor :name, :vegetarian
   # Instantiate a new topping
   #
   #name - The String name of the topping
   #
   # Returns a new Topping object
 
-  def initialize(name)
+  def initialize(name, vegetarian: false)
     @name = name
+    @vegetarian = vegetarian
   end
 
   def method_name
