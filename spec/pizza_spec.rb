0fd81b647f61fc3ed12e4b489d64f7be6ee9619f
Ian White <ianwhite@utexas.edu>
2014-02-10 11:26:41 -0600

    Add test for Pizza.initialize to accept array toppings and set it equal to @toppings with attr_accessor :toppings

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -2,7 +2,19 @@ require './pizza'
 
 describe Pizza do
   it "exists" do
-    expect(Pizza).to be_a(Class)
+      expect(Pizza).to be_a(Class)
+    end
+
+  describe '.initialize' do
+    it 'records all of the toppings' do
+      toppings = [
+        Topping.new('mushrooms', vegetarian: true),
+        Topping.new('pepperoni')
+      ]
+      pizza = Pizza.new(toppings)
+
+      expect(pizza.toppings).to eq(toppings)
+    end
   end
 end
 
@@ -24,4 +36,6 @@ describe Topping do
 
       expect(topping.vegetarian).to eq(true)
     end
+
+
 end
